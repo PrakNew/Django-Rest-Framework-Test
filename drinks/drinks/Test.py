937733ab63c319e525d76b2528df199283c41b2e
PrakNew <prakharnewatia97@gmail.com>
2022-10-12 20:06:49 +0530

    added test.py to test our work

--- drinks/drinks/Test.py
+++ drinks/drinks/Test.py
@@ -1,21 +1,28 @@
+# sourcery skip: avoid-builtin-shadow
 import requests
 import json
-# Get all method
+# Get all method ---------------GET
 response = requests.get('http://127.0.0.1:8000/drinks')
 print(response.json())
 
-#adding a new data into the database
-# d={'name':'test1','description':'test description'}
-# response = requests.post('http://127.0.0.1:8000/drinks/',data=d)
-# print(response.json())
+#adding a new data into the database ------------ POST 
+d={'name':'test1','description':'test description'}
+response = requests.post('http://127.0.0.1:8000/drinks/',data=d)
+print(response.json())
 
 id = int(input())
-# response=requests.get('http://127.0.0.1:8000/drinks/'+str(id))
-# if response.status_code==200:
-#     print(response.json())
-# d={'name':'testput','description':'testput description'}
-# response = requests.put('http://127.0.0.1:8000/drinks/'+str(id),data =d)
-# if response.status_code==200:
-#     print(response.json())
+#---------------GET
+response=requests.get('http://127.0.0.1:8000/drinks/'+str(id))
+if response.status_code==200:
+    print(response.json())
 
+#-------------PUT
+d={'name':'testput','description':'testput description'}
+response = requests.put('http://127.0.0.1:8000/drinks/'+str(id),data =d)
+if response.status_code==200:
+    print(response.json())
+
+#---------------DELETE
+response=requests.delete('http://127.0.0.1:8000/drinks/'+str(id))
+print(response.text)
 
